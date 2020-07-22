f9d8b88a2be213d6a842e3816cc00d26a441383a
xavysp <xsoriap@yahoo.com>
2020-07-22 04:42:36 +0200

    The ultimate DexiNed-Pytorch model

--- DexiNed-Pytorch/model.py
+++ DexiNed-Pytorch/model.py
@@ -18,7 +18,7 @@ class _DenseLayer(nn.Sequential):
     def forward(self, x):
         x1, x2 = x
         # maybe I should put here a RELU
-        new_features = super(_DenseLayer, self).forward(x1) # F.relu()
+        new_features = super(_DenseLayer, self).forward(F.relu(x1)) # F.relu()
         return 0.5 * (new_features + x2), x2
 
 class _DenseBlock(nn.Sequential):
@@ -75,17 +75,24 @@ class UpConvBlock(nn.Module):
         return self.features(x)
 
 class SingleConvBlock(nn.Module):
-    def __init__(self, in_features, out_features, stride):
+    def __init__(self, in_features, out_features, stride, use_bs=True):
         super(SingleConvBlock, self).__init__()
+        self.use_bn=True
         self.conv = nn.Conv2d(in_features, out_features, 1, stride=stride)
         self.bn = nn.BatchNorm2d(out_features)
 
     def forward(self, x):
-        return self.bn(self.conv(x))
+        x = self.conv(x)
+        if self.use_bn:
+            x = self.bn(x)
+
+        return x
 
 class DoubleConvBlock(nn.Module):
-    def __init__(self, in_features, mid_features,out_features=None, stride=1):
+    def __init__(self, in_features, mid_features,out_features=None, stride=1,
+                 use_act=True):
         super(DoubleConvBlock, self).__init__()
+        self.use_act=use_act
         if out_features is None:
             out_features = mid_features
         self.conv1 = nn.Conv2d(
@@ -101,15 +108,16 @@ class DoubleConvBlock(nn.Module):
         x = self.relu(x)
         x = self.conv2(x)
         x = self.bn2(x)
-        x = self.relu(x)
+        if self.use_act:
+            x = self.relu(x)
         return x
 
 class DexiNet(nn.Module):
     """ Definition of the DXtrem network. """
     def __init__(self):
         super(DexiNet, self).__init__()
-        self.block_1 = DoubleConvBlock(3, 32, 64, stride=2)
-        self.block_2 = DoubleConvBlock(64, 128)
+        self.block_1 = DoubleConvBlock(3, 32, 64, stride=2,)
+        self.block_2 = DoubleConvBlock(64, 128, use_act=False)
         self.dblock_3 = _DenseBlock(2, 128, 256)
         self.dblock_4 = _DenseBlock(3, 256, 512)
         self.dblock_5 = _DenseBlock(3, 512, 512)
@@ -122,10 +130,10 @@ class DexiNet(nn.Module):
         self.side_4 = SingleConvBlock(512, 512, 1)
         self.side_5 = SingleConvBlock(512, 256, 1)
 
-        self.pre_dense_2 = SingleConvBlock(128, 256, 2) # by me, for left skip block4
+        self.pre_dense_2 = SingleConvBlock(128, 256, 2, use_bs=False) # by me, for left skip block4
         self.pre_dense_3 = SingleConvBlock(128, 256, 1)
         self.pre_dense_4 = SingleConvBlock(256, 512, 1)
-        self.pre_dense_5_0 = SingleConvBlock(256, 512, 2)
+        self.pre_dense_5_0 = SingleConvBlock(256, 512, 2,use_bs=False)
         self.pre_dense_5 = SingleConvBlock(512, 512, 1)
         self.pre_dense_6 = SingleConvBlock(512, 256, 1)
 
